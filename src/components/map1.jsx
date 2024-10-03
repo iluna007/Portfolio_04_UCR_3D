38376c5755be1c292b03fe8d4847bd85878477a6
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-10-03 16:50:12 +0100

    icons

--- src/components/map1.jsx
+++ src/components/map1.jsx
@@ -17,6 +17,15 @@ const Map_1 = () => {
     );
 
     // Define the base layers
+    const leafletIcon = L.divIcon({
+      html: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 256 256">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg>`,
+      className: "",
+    });
+
     const basemap = L.tileLayer(
       "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       {
@@ -47,12 +56,12 @@ const Map_1 = () => {
     basemap.addTo(mapRef.current);
 
     // Add markers to the map
-    const marker1 = L.marker([9.889718, -84.037271]).bindPopup(
-      "Entrada A del parque"
-    );
-    const marker2 = L.marker([9.889955333454145, -84.03392571430172]).bindPopup(
-      "Entrada B del parque"
-    );
+    const marker1 = L.marker([9.889718, -84.037271], {
+      icon: leafletIcon,
+    }).bindPopup("Entrada A del parque");
+    const marker2 = L.marker([9.889955333454145, -84.03392571430172], {
+      icon: leafletIcon,
+    }).bindPopup("Entrada B del parque");
 
     // Add a popup to the map
     const popup = L.popup();
